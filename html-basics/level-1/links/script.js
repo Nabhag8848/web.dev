3a377af4be1b694f6b5d4e1f178b16891de73d20
Nabhag Motivaras <65061890+Nabhag8848@users.noreply.github.com>
2025-09-24 10:43:46 +0530

    painting app, link tracker - sept23

---
+++ html-basics/level-1/links/script.js
@@ -0,0 +1,27 @@
+const canvas = document.querySelector("canvas");
+const c = canvas.getContext("2d");
+
+c.fillStyle = "black";
+
+let isDrawing;
+
+function draw(x, y) {
+  if (isDrawing) {
+    c.beginPath();
+    c.arc(x, y, 10, 0, Math.PI * 2);
+    c.closePath();
+    c.fill();
+  }
+}
+
+canvas.addEventListener("mousemove", (event) => {
+  draw(event.offsetX, event.offsetY);
+});
+
+canvas.addEventListener("mousedown", () => (isDrawing = true));
+canvas.addEventListener("mouseup", () => (isDrawing = false));
+
+document.querySelector("a").addEventListener("click", (event) => {
+  console.log(canvas.toDataURL());
+  event.target.href = canvas.toDataURL();
+});
